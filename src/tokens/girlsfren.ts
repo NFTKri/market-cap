dff4d595a74a606b46fbc4010aec40186fdf38c6
NFTKri <166889251+NFTKri@users.noreply.github.com>
2024-06-24 00:36:35 -0400

    Update and rename girlfren.ts to girlsfren.ts

--- src/tokens/girlsfren.ts
+++ src/tokens/girlsfren.ts
@@ -2,16 +2,16 @@
 import { defaultFetcherOptions, SupplyFetcher } from "../types";
 import { getAmountInAddresses, getBlockFrostInstance } from "../utils";
 
-const GIRLFREN = "20efaf13727eea5813e57530f1f9bb60fa8a986056b48c4b89d59b414769726c4672656e"";
+const GIRLsFREN = "20efaf13727eea5813e57530f1f9bb60fa8a986056b48c4b89d59b414769726c4672656e"";
 
 const fetcher: SupplyFetcher = async (options = defaultFetcherOptions) => {
   const blockFrost = getBlockFrostInstance(options);
   const total = 69_420_000_000;
-  const treasuryRaw = await getAmountInAddresses(blockFrost, GIRLFREN, [
+  const treasuryRaw = await getAmountInAddresses(blockFrost, GIRLsFREN, [
     "stake1uyz9dgwqjdtqa36nn7mn4t6ptsu66dnd3c9uwv9luhkzlcgsa3v5h", // Treasury
   ]);
 
-  const burnRaw = await getAmountInAddresses(blockFrost, GIRLFREN, [
+  const burnRaw = await getAmountInAddresses(blockFrost, GIRLsFREN, [
     "addr1w8qmxkacjdffxah0l3qg8hq2pmvs58q8lcy42zy9kda2ylc6dy5r4", // burn address
   ]);
 
